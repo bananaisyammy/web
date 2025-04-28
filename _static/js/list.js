edd55b2c8e59ee1dc4cfa53b6a17f989d25fcbe1
bananaisyammy <Letsjump@outlook.jp>
2025-04-28 19:19:28 +0900

    .staticを_staticに

--- _static/js/list.js
+++ _static/js/list.js
@@ -1,7 +1,7 @@
 var list_item;
 run()
 async function run(){
-    list_item = await fetch("/web/.static/html/list_item.html").then(response => response.text());
+    list_item = await fetch("/web/_static/html/list_item.html").then(response => response.text());
     for (let i = 0; i < 20; i++) {
         document.querySelector("#products").appendChild(createElementFromHTML(list_item));
     }
