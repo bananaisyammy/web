d2c9bf2ab47c10d0a89cd1948396c1806396ec44
bananaisyammy <Letsjump@outlook.jp>
2025-05-02 20:35:40 +0900

    ほぼ完成

--- _static/js/list.js
+++ _static/js/list.js
@@ -1,9 +1,30 @@
-var list_item;
-run()
-async function run(){
-    list_item = await fetch("/web/_static/html/list_item.html").then(response => response.text());
-    for (let i = 0; i < 20; i++) {
-        document.querySelector("#products").appendChild(createElementFromHTML(list_item));
+//クエリパラメータから作りたいリストを得る
+console.log(new URL(document.currentScript.src).searchParams.get("list"));
+var listContents = JSON.parse(new URL(decodeURI(document.currentScript.src)).searchParams.get("list"));
+//itemの原型をfetch
+var listItem;
+itemFetch();
+async function itemFetch(){
+    listItem = await fetch("/web/_static/html/list_item.html").then(response => response.text());
+    for (const content of listContents) {
+        let child = listItem.replace("%description%",content["description"])
+            .replace("%title%",content["title"])
+            .replace("%url%",content["url"])
+            .replace("%imageSrc%",content["imageSrc"])
+            .replace("%creatorName%",content["creatorName"])
+        document.querySelector("#list").appendChild(createElementFromHTML(child));
     }
 }
 
+
+/*
+<書式>
+[
+{
+    "description": "説明文",
+    "title": "タイトル",
+    "url": "URL",
+    "imageSrc": "画像URL",
+    "creatorName": "作成者名"
+}, ... ]
+*/
