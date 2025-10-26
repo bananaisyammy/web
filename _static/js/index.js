bcefe968c9d6a9f0fed70e3ee8135a53a245e300
bananaisyammy <Letsjump@outlook.jp>
2025-10-26 18:11:28 +0900

    これでどうだ

--- _static/js/index.js
+++ _static/js/index.js
@@ -1,7 +1,11 @@
 window.addEventListener("load", async (event) => {
     console.log("ページが完全に読み込まれました");
-    
     document.head.innerHTML = await fetch("/web/_static/html/head.html").then(response => response.text());
+    const s = document.createElement("script");
+    s.defer = true;
+    s.src = "https://static.cloudflareinsights.com/beacon.min.js";
+    s.setAttribute("data-cf-beacon", '{"token": "31653fcda84d4298a7ad3fd52126b073"}');
+    document.head.appendChild(s); // bodyの最後に追加
     document.querySelector("#header").innerHTML = await fetch("/web/_static/html/header.html").then(response => response.text());
     document.querySelector("#footer").innerHTML = await fetch("/web/_static/html/footer.html").then(response => response.text());
 });
