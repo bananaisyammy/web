f6dbeba2fd4c33a0cfe0aff251f9d86d9793c13a
bananaisyammy <Letsjump@outlook.jp>
2025-11-03 08:57:35 +0900

    とちゅう

--- test/menutest2/index.js
+++ test/menutest2/index.js
@@ -5,32 +5,46 @@ const heightPercent = window.innerHeight/1440;
 
 window.onload = function () {
     //imgクリック時に関数を呼び出す
-    document.getElementById("nextButton").addEventListener("click", onNextButtonClick);
+    this.document.getElementById("nextButton").addEventListener("click", onNextButtonClick);
     document.getElementById("backButton").addEventListener("click", onBackButtonClick);
-
-    //背景画像設定
-    document.body.style.backgroundImage = "url('menuBg.png')";
-    this.document.body.style.backgroundSize = "cover";
+    document.getElementById("contactButton").addEventListener("click", onContactButtonClick);
+    //button定義
+    contactButton = this.document.getElementById("contactButton")
+    nextButton = this.document.getElementById("nextButton")
+    backButton = this.document.getElementById("backButton")
 
     //nextButton位置調整
-    nextButton = this.document.getElementById("nextButton")
     nextButton.style.width = 150 * widthPercent + "px";
     nextButton.style.height = 165 * heightPercent + "px";
-    nextButton.style.position = "absolute";
     nextButton.style.left = 1575 * widthPercent + "px";
     nextButton.style.top = 660 * heightPercent + "px";
 
     //backButton位置調整
-    backButton = this.document.getElementById("backButton")
     backButton.style.width = 165 * widthPercent + "px";
     backButton.style.height = 180 * heightPercent + "px";
-    backButton.style.position = "absolute";
     backButton.style.left = 180 * widthPercent + "px";
     backButton.style.top = 660 * heightPercent + "px";
+
+    //contactButton位置調整
+    contactButton.style.width = 165 * widthPercent + "px";
+    contactButton.style.height = 270 * heightPercent + "px";
+    contactButton.style.left = 1560 * widthPercent + "px";
+    contactButton.style.top = 345 * heightPercent + "px";
+    //lightshadow位置調整
+
+    this.document.querySelectorAll(".tabs").forEach((tab, index) => {
+        tab.style.width = 285 * widthPercent + "px";
+        tab.style.height = 50 * heightPercent + "px";
+        tab.style.left = (360 + index * 300) * widthPercent + "px";
+        tab.style.top = 135 * heightPercent + "px";
+    });
 }
 function onNextButtonClick() {
     alert("Next button clicked!");
 }
 function onBackButtonClick() {
     alert("Back button clicked!");
+}
+function onContactButtonClick() {
+    alert("Contact button clicked!");
 }
