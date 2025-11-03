63ee02f66f76e94cf090a2dcaaeec46314fee1a6
bananaisyammy <Letsjump@outlook.jp>
2025-11-03 14:45:34 +0900

    できたンゴ

--- test/menutest2/index.js
+++ test/menutest2/index.js
@@ -2,12 +2,14 @@ let nextButton;
 let backButton;
 const widthPercent = window.innerWidth/1920;
 const heightPercent = window.innerHeight/1440;
+let tabIndex = 0;
 
 window.onload = function () {
     //imgクリック時に関数を呼び出す
     this.document.getElementById("nextButton").addEventListener("click", onNextButtonClick);
     document.getElementById("backButton").addEventListener("click", onBackButtonClick);
     document.getElementById("contactButton").addEventListener("click", onContactButtonClick);
+    
     //button定義
     contactButton = this.document.getElementById("contactButton")
     nextButton = this.document.getElementById("nextButton")
@@ -30,14 +32,48 @@ window.onload = function () {
     contactButton.style.height = 270 * heightPercent + "px";
     contactButton.style.left = 1560 * widthPercent + "px";
     contactButton.style.top = 345 * heightPercent + "px";
-    //lightshadow位置調整
 
+    //tab位置調整
     this.document.querySelectorAll(".tabs").forEach((tab, index) => {
-        tab.style.width = 285 * widthPercent + "px";
-        tab.style.height = 50 * heightPercent + "px";
-        tab.style.left = (360 + index * 300) * widthPercent + "px";
-        tab.style.top = 135 * heightPercent + "px";
+        tab.style.height = 180 * heightPercent + "px";
+        tab.style.top = 165 * heightPercent + "px";
+        tab.addEventListener("click", () => onTabClick(index));
     });
+
+
+    this.document.getElementById("tab0").style.width = 285 * widthPercent + "px";
+    this.document.getElementById("tab0").style.left = 360 * widthPercent + "px";
+    this.document.getElementById("tab0").style.backgroundColor = "#15474B";
+
+    this.document.getElementById("tab1").style.width = 285 * widthPercent + "px";
+    this.document.getElementById("tab1").style.left = 660 * widthPercent + "px";
+    this.document.getElementById("tab1").style.backgroundColor = "#207070";
+
+    this.document.getElementById("tab2").style.width = 285 * widthPercent + "px";
+    this.document.getElementById("tab2").style.left = 960 * widthPercent + "px";
+    this.document.getElementById("tab2").style.backgroundColor = "#207070";
+    
+    this.document.getElementById("tab3").style.width = 300 * widthPercent + "px";
+    this.document.getElementById("tab3").style.left = 1260 * widthPercent + "px";
+    this.document.getElementById("tab3").style.backgroundColor = "#207070";
+
+    //shadow位置調整
+    this.document.querySelectorAll(".shadow").forEach((shadow, index) => {
+        shadow.style.width = 1920 * widthPercent + "px";
+        shadow.style.height = 1440 * heightPercent + "px";
+        shadow.style.left = (0) * widthPercent + "px";
+        shadow.style.top = 0 * heightPercent + "px";
+    });
+
+}
+
+function onTabClick(index) {
+    console.log("Tab " + index + " clicked!");
+    // タブの背景色を更新
+    document.querySelectorAll(".tabs").forEach((tab, i) => {
+        tab.style.backgroundColor = (i === index) ? "#15474B" : "#207070";
+    });
+    tabIndex = index;
 }
 function onNextButtonClick() {
     alert("Next button clicked!");
@@ -46,5 +82,6 @@ function onBackButtonClick() {
     alert("Back button clicked!");
 }
 function onContactButtonClick() {
-    alert("Contact button clicked!");
+    // iframe内からparent（親ウィンドウ）のURLを変更
+    window.parent.location.href = "/web/contact/";
 }
